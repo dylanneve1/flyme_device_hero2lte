c489c99d28c34c86a6837711a9d57c0d8bec7243
dylanneve1 <dylanneve1@gmail.com>
2018-03-27 12:20:38 +0100

    Replace with full hero2lte files

--- vendor/system/bin/install-recovery.sh
+++ vendor/system/bin/install-recovery.sh
@@ -1,9 +1,9 @@
 #!/system/bin/sh
 if [ -f /system/etc/recovery-transform.sh ]; then
-  exec sh /system/etc/recovery-transform.sh 41109504 339e6f4eb28871ae40507d49c596e7e0d0a2b552 33763328 1b37bb19db56040b2ef36eb9d860dda7f8af5901
+  exec sh /system/etc/recovery-transform.sh 41132032 d8401349a0a6a6d6a544859e0a09e08add6e24cc 33785856 24649b738992c2fa4bacdbabed8aef11c4391a6c
 fi
-if ! applypatch -c EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY:41109504:339e6f4eb28871ae40507d49c596e7e0d0a2b552; then
-  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/platform/155a0000.ufs/by-name/BOOT:33763328:1b37bb19db56040b2ef36eb9d860dda7f8af5901 EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY 339e6f4eb28871ae40507d49c596e7e0d0a2b552 41109504 1b37bb19db56040b2ef36eb9d860dda7f8af5901:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+if ! applypatch -c EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY:41132032:d8401349a0a6a6d6a544859e0a09e08add6e24cc; then
+  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/platform/155a0000.ufs/by-name/BOOT:33785856:24649b738992c2fa4bacdbabed8aef11c4391a6c EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY d8401349a0a6a6d6a544859e0a09e08add6e24cc 41132032 24649b738992c2fa4bacdbabed8aef11c4391a6c:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
   log -t recovery "Recovery image already installed"
 fi
